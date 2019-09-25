ee788eeea40f5d663d5dcf6b1c8e29829d80eeec
Naume-k <kobnaome@gmail.com>
2019-09-25 11:56:17 +0200

    heroku deploy

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY ='arandomstring'
+export SECRET_KEY ='a random variable1'
 export MAIL_USERNAME='kobnaome@gmail.com'
 export MAIL_PASSWORD='naome4christ@1994'
     python3.6 manage.py server
