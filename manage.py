08638d1a23da1ed36a5bf335891fa6b33a1a06ed
Naume-k <kobnaome@gmail.com>
2019-09-25 10:15:14 +0200

    corrected the indentention

--- manage.py
+++ manage.py
@@ -3,13 +3,8 @@ from app import create_app,db
 from app.models import User,Role,Pitch
 from  flask_migrate import Migrate, MigrateCommand
 
-
-
-# Creating app instance
 app = create_app('production')
 app = create_app('development')
-# app = create_app('test')
-
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)
@@ -24,7 +19,6 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User, Role = Role,Review=Review)
-
 
 if __name__ == '__main__':
     manager.run()
