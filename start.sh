d874ee0bf458ca02141804585a9a6963022ea106
Naume-k <kobnaome@gmail.com>
2019-09-25 10:12:16 +0200

    added the keys to be exported

---
+++ start.sh
@@ -0,0 +1,5 @@
+# export MOVIE_API_KEY='ff73536b7f0e63d45fe4f61ecf8eb703'
+export SECRET_KEY ='arandomstring'
+export MAIL_USERNAME='kobnaome@gmail.com'
+export MAIL_PASSWORD='naome4christ@1994'
+    python3.6 manage.py server
